75fbce808b8180d192919a71cb918a33c7a1b663
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-14 12:11:02 -0400

    frameless and unresizable

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const { app, BrowserWindow }  = electron;
 let mainWindow;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+    height: 500,
+    width: 300,
+    frame: false,
+    resizable: false
+  });
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 
 })
