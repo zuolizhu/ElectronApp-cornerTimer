8d46daaab8a8e91513a4f884e8d8338f62c38d81
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-16 16:58:17 -0400

    seting position width bounds

--- index.js
+++ index.js
@@ -22,10 +22,25 @@ app.on('ready', () => {
 
   tray = new Tray(iconPath);
 
-  tray.on('click', () => {
+  tray.on('click', (e, bounds) => {
+
+    //Click event bounds
+    const { x, y } = bounds;
+
+    //window height and width
+    const { height, width } = mainWindow.getBounds();
+
     if (mainWindow.isVisible()) {
       mainWindow.hide();
     } else {
+      const xPosition = process.platform === 'darwin' ? x - width / 2 : x - width / 3;
+      const yPosition = process.platform === 'darwin' ? y : y - height;
+      mainWindow.setBounds({
+        x: xPosition,
+        y: yPosition,
+        height,
+        width
+      });
       mainWindow.show();
     }
   });
