4eb56a7f957acb8b84b0c0c3b5f94ab2f58650a2
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-16 16:27:35 -0400

    toggle to show or hide the mainwindow by click

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const electron = require('electron');
 const { app, BrowserWindow, Tray }  = electron;
 
 let mainWindow;
+let tray;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     height: 500,
     width: 300,
     frame: false,
-    resizable: false
+    resizable: false,
+    show: false
   });
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 
@@ -18,7 +20,15 @@ app.on('ready', () => {
 
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
 
-  new Tray(iconPath);
+  tray = new Tray(iconPath);
+
+  tray.on('click', () => {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      mainWindow.show();
+    }
+  });
 
 
 })
