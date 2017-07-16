4912fa6e21f28076be5e135619a67d43c4e82394
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-16 19:19:49 -0400

    refactor main part into small peices

---
+++ app/timerTray.js
@@ -0,0 +1,36 @@
+const electron = require('electron');
+const { Tray } = electron;
+
+class TimerTray extends Tray {
+  constructor(iconPath, mainWindow) {
+    super(iconPath);
+    this.mainWindow = mainWindow;
+    this.on('click', this.handleClick.bind(this));
+  }
+
+  handleClick(e, bounds) {
+
+    //Click event bounds
+    const { x, y } = bounds;
+
+    //window height and width
+    const { height, width } = this.mainWindow.getBounds();
+
+    if (this.mainWindow.isVisible()) {
+      this.mainWindow.hide();
+    } else {
+      const xPosition = process.platform === 'darwin' ? x - width / 2 : x - width / 3;
+      const yPosition = process.platform === 'darwin' ? y : y - height;
+      this.mainWindow.setBounds({
+        x: xPosition,
+        y: yPosition,
+        height,
+        width
+      });
+      this.mainWindow.show();
+    }
+  }
+}
+
+
+module.exports = TimerTray;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const electron = require('electron');
+const TimerTray = require('./app/timerTray');
 
 const { app, BrowserWindow, Tray }  = electron;
 
@@ -20,30 +21,6 @@ app.on('ready', () => {
 
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
 
-  tray = new Tray(iconPath);
-
-  tray.on('click', (e, bounds) => {
-
-    //Click event bounds
-    const { x, y } = bounds;
-
-    //window height and width
-    const { height, width } = mainWindow.getBounds();
-
-    if (mainWindow.isVisible()) {
-      mainWindow.hide();
-    } else {
-      const xPosition = process.platform === 'darwin' ? x - width / 2 : x - width / 3;
-      const yPosition = process.platform === 'darwin' ? y : y - height;
-      mainWindow.setBounds({
-        x: xPosition,
-        y: yPosition,
-        height,
-        width
-      });
-      mainWindow.show();
-    }
-  });
-
+  tray = new TimerTray(iconPath, mainWindow);
 
 })
