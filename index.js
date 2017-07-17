59382c5a5ccd1be76c41256fa1f23035dd7d0c86
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-16 22:24:25 -0400

    Mac OS corner time display
    Windows 10 balloon is a little annoying

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const MainWindow = require('./app/mainWindow');
 const TimerTray = require('./app/timerTray');
 
 
-const { app, BrowserWindow, Tray }  = electron;
+const { app, ipcMain }  = electron;
 
 let mainWindow;
 let cornerTray;
@@ -19,5 +19,19 @@ app.on('ready', () => {
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
 
   cornerTray = new TimerTray(iconPath, mainWindow);
-
 })
+
+
+ipcMain.on('update-timer', (e, timeLeft) => {
+  const iconName = process.platform == 'win32' ? 'windows-icon@2x.png' : 'iconTemplate.png';
+  const iconPath = path.join(__dirname, './src/assets/' + iconName);
+  if(process.platform == 'darwin') {
+    cornerTray.setTitle(timeLeft);
+  } else {
+    // cornerTray.displayBalloon({
+    //   icon: iconPath,
+    //   title: '',
+    //   content: timeLeft
+    // })
+  }
+});
